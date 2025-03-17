36659cacdadd2c9cb2f96a4aeee04b8159158179
mrakib4 <mrakib4@my.centennialcollege.ca>
2025-03-17 00:33:21 -0400

    Initial commit

---
+++ MVCAgile Process/Data/MVCAgile_ProcessContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCAgile_Process.Models;
+
+namespace MVCAgile_Process.Data
+{
+    public class MVCAgile_ProcessContext : DbContext
+    {
+        public MVCAgile_ProcessContext (DbContextOptions<MVCAgile_ProcessContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCAgile_Process.Models.Movie> Movie { get; set; } = default!;
+    }
+}
